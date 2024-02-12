bab4a4474b19dd8720b87e454dc13c0228d4bc56
Danilo2801 <danilo.gomes.santana94@gmail.com>
2024-02-12 17:52:52 -0300

    Acertando os níveis e finalizando projeto com a saída

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let nomeHeroi = "Ronaldo"
-let xpHeroi = 5000
+let xpHeroi = 3800
 
 if (xpHeroi < 1000) {
     nivel = "Ferro"
@@ -13,15 +13,17 @@ else if (xpHeroi >= 2001 && xpHeroi <= 3000) {
 else if (xpHeroi >= 3001 && xpHeroi <= 4000) {
     nivel = "Ouro"
 }
-else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+else if (xpHeroi >= 4001 && xpHeroi <= 5000) {
     nivel = "Platina"
 }
-else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+else if (xpHeroi >= 5001 && xpHeroi <= 6000) {
     nivel = "Ascendente"
 }
-else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
     nivel = "Imortal"
 }
 else {
     nivel = "Radiante"
 }
+
+console.log("O herói de nome " + nomeHeroi + " está no nível de " + nivel)
