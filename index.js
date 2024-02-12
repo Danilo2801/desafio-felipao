775e9ddf0ecd2d774a85e774f00bb422fd78cfb4
Danilo2801 <danilo.gomes.santana94@gmail.com>
2024-02-12 17:46:10 -0300

    Colocando os níveis do heroi

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 let nomeHeroi = "Ronaldo"
 let xpHeroi = 5000
 
-if (xpHeroi < 1000){
+if (xpHeroi < 1000) {
     nivel = "Ferro"
 }
-console.log("O nivel do " + nomeHeroi + " é " + xpHeroi)
+else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze"
+}
+else if (xpHeroi >= 2001 && xpHeroi <= 3000) {
+    nivel = "Prata"
+}
+else if (xpHeroi >= 3001 && xpHeroi <= 4000) {
+    nivel = "Ouro"
+}
+else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Platina"
+}
+else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Ascendente"
+}
+else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Imortal"
+}
+else {
+    nivel = "Radiante"
+}
